4041043fb39c5ee29503ec98f19b62336d3755c2
Endermandels <elijahdelavar@gmail.com>
2024-11-05 10:54:29 -0800

    Update venv pip, run flask

--- hello.py
+++ hello.py
@@ -1,7 +1,26 @@
 """
 Tutorial from Codemy.com on YouTube
     https://youtu.be/0Qxtt4veJIc?si=62kyEF-3QPqfB3yn
+
+$env:FLASK_ENV="development" (enables development env)
+$env:FLASK_DEBUG="True" (enables live feedback from source code changes)
+$env:FLASK_APP="hello.py" (tells flask which file is the main app)
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
+# Create Flask Instance
+app = Flask(__name__)
+
+# Create a route decorator
+@app.route('/')
+
+def index():
+    return "<h1>Hello World!</h1>"
+
+
+
+@app.route('/user/<name>')
+
+def user(name):
+    return f"<h1>Bye, {name}</h1>"
